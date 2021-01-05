6cbac3a407c25a332ec5dca8a456233badc1d1c0
Damola <mohammeddam1@outlook.com>
2021-01-05 18:40:41 -0500

    Update server.py

--- server.py
+++ server.py
@@ -16,19 +16,22 @@ class Server:
 		self.addr = (self.host, self.port)
 
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		print(SERVER, "Server is starting")
 		self.server.bind(self.addr)
 		try:
 			self.server.listen(self.maxPeople)
 		except socket.error as e:
 			print(ERROR, e)
+		
+		print(SERVER, "Server is listening on", self.host, "on port", self.port)
 
 	def connectedClient(self, conn, addr):
 		try:
 			while 1:
 				data = conn.recv(self.byteSize).decode(self.encoding)
+				print(f"{addr}: {data}")
 				if not data:
 					break
-				print(data)
 				conn.sendall(data.encode(self.encoding))
 		except socket.error as e:
 			print(ERROR, e)
